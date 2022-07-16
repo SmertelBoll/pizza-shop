3b6b0f2b2901a247a0cb0f34070f7975cfc5cf2b
Lyubomyr <sholop2113@gmail.com>
2022-07-16 20:22:54 +0300

    the website header is ready

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Button() {
+   return (
+      <button class="header-button">
+         <div className="header-button__price">520 ₴</div>
+         <div className="header-button__number-and-icon">
+            <img src="img/iconfinder_shopping-cart.svg" alt="" className="header-button__icon" width={16} height={16} />
+            <p className="header-button__number">3</p>
+         </div>
+      </button>
+   )
+}
+
+export default Button
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button'
+
+function Header() {
+   return (
+      <header class="header">
+         <div class="header__logo-and-name">
+            <img src="img/logo.png" alt="" className="header__logo" width={38} height={38} />
+
+            <div>
+               <h1 className="header__name">REACT PIZZA</h1>
+               <p className="header__description">Найсмачніша піца у світі</p>
+            </div>
+         </div>
+         <Button />
+      </header>
+   )
+}
+
+export default Header
