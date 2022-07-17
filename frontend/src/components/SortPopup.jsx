c8fea79a4e6b8269d31452cf42ecac6dba7e48b2
Lyubomyr <sholop2113@gmail.com>
2022-07-17 19:16:53 +0300

    A sorting block has been created. The functionality of opening and closing popup has been added

--- frontend/src/components/SortPopup.jsx
+++ frontend/src/components/SortPopup.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function SortPopup() {
+   let sortLabel = ["популярності", "ціні", "алфавіту"]
+   const [activeLabel, setActiveLabel] = useState(sortLabel[0])
+   const [visiblePopup, setVisiblePopup] = useState(false)
+
+   let toggleVisiblePopup = () => {
+      setVisiblePopup(!visiblePopup)
+   }
+
    return (
-      <div>SortPopup</div>
+      <div className="sort-popup">
+         {
+            visiblePopup
+               ? <b className="sort-popup__sort-by sort-popup__triangle-inactive">Сортувати по: </b>
+               : <b className="sort-popup__sort-by sort-popup__triangle-active">Сортувати по: </b>
+         }
+         <button className="sort-popup__label" onClick={toggleVisiblePopup}>{activeLabel}</button>
+         <ul className={`sort-popup__popup ${visiblePopup && 'sort-popup__popup-active'}`}>
+            {
+               sortLabel.map((name) => (
+                  activeLabel === name
+                     ? <li className="sort-popup__popup-item sort-popup__item-active">{name}</li>
+                     : <li className="sort-popup__popup-item">{name}</li>
+               ))
+            }
+         </ul>
+      </div>
    )
 }
 
