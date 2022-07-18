7667bdebd163a84454853041a54196b6ea1b77de
Lyubomyr <sholop2113@gmail.com>
2022-07-18 17:21:06 +0300

    added a pizza layout

---
+++ frontend/src/components/PizzaBlock.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function PizzaBlock() {
+   return (
+      <div className="pizza-block">
+         <img src="https://dodopizza.azureedge.net/static/Img/Products/f035c7f46c0844069722f2bb3ee9f113_584x584.jpeg" alt="" className="pizza-block__img" width={260} height={260} />
+         <h3 className="pizza-block__title">Чізбургер-піца</h3>
+         <div className="pizza-block__choice-types">
+            <button className="pizza-block__type">тонка</button>
+            <button className="pizza-block__type">традиційна</button>
+         </div>
+         <div className="pizza-block__price-and-button">
+            <div className="pizza-block__price">від 100 ₴</div>
+            <button className="pizza-block__button">
+               <span className="pizza-block__button-text">Добавити</span>
+               <div className="pizza-block__button-counter"><span>0</span></div>
+            </button>
+         </div>
+      </div>
+   )
+}
+
+export default PizzaBlock
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import PizzaBlock from '../components/PizzaBlock'
 
 function Home() {
    return (
-      <div>Home</div>
+      <div className="home">
+         <h2 className="home__label">Всі піци</h2>
+         <div className="home__pizzas">
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+            <PizzaBlock />
+         </div>
+      </div>
    )
 }
 
